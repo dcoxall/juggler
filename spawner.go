f543e6e826c2b6009eba91b9f6c0c9d98aabf225
Darren Coxall <darren.coxall@simplybusiness.co.uk>
2015-02-13 09:44:58 +0000

    First signs of spawner type
    The Spawner is responsible for building the instance type and managing
    access to the instances created. It does not start or stop instances
    however.

---
+++ spawner.go
@@ -0,0 +1,39 @@
+package juggler
+
+import "errors"
+
+type Spawner struct {
+	generator func(string) (Instancer, error)
+	instances map[string]Instancer
+}
+
+var (
+	InstanceNotFoundError = errors.New("Unable to locate instance")
+)
+
+func NewSpawner(gen func(string) (Instancer, error)) *Spawner {
+	return &Spawner{
+		generator: gen,
+		instances: make(map[string]Instancer),
+	}
+}
+
+func (s *Spawner) Fetch(reference string) (Instancer, error) {
+	if i, err := s.getInstance(reference); err != nil {
+		if i, err = s.generator(reference); err == nil {
+			s.instances[reference] = i
+		}
+		return i, err
+	} else {
+		return i, err
+	}
+}
+
+func (s *Spawner) getInstance(reference string) (Instancer, error) {
+	for key, instance := range s.instances {
+		if key == reference {
+			return instance, nil
+		}
+	}
+	return nil, InstanceNotFoundError
+}
---
+++ spawner_test.go
@@ -0,0 +1,33 @@
+package juggler
+
+import (
+	"testing"
+)
+
+func TestSpawnerInstanceGeneration(t *testing.T) {
+	generated := false
+	generator := func(reference string) (Instancer, error) {
+		generated = true
+		return new(Instance), nil
+	}
+	spawner := NewSpawner(generator)
+	spawner.Fetch("foobar")
+	if !generated {
+		t.Errorf("Spawner didn't generate new instance with generator function")
+	}
+}
+
+func TestSpawnerInstanceStorage(t *testing.T) {
+	generated := 0
+	generator := func(reference string) (Instancer, error) {
+		generated += 1
+		return new(Instance), nil
+	}
+	spawner := NewSpawner(generator)
+	spawner.Fetch("foobar") // generates a new instance
+	spawner.Fetch("barfoo") // generates a new instance
+	spawner.Fetch("foobar") // uses previously generated instance
+	if generated != 2 {
+		t.Errorf("Expected to only generate twice but got %d", generated)
+	}
+}
